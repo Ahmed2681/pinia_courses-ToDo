ae87f75b005f5dbd333228553e4c7568ee10e5dc
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-03-26 03:41:57 +0300

    wip

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { useTodoListStore } from "@/stores/todoList.js";
+import { storeToRefs } from "pinia";
 
-<template></template>
+const store = useTodoListStore();
+
+const { todoList } = storeToRefs(store);
+</script>
+
+<template>
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+      <span>{{ todo.item }}</span>
+    </div>
+  </div>
+</template>
 
 <style scoped></style>
