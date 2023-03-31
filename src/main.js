46d9556dbdaf9c6dee2a24a0330e4de8f4452f48
Ahmed <102857181+Ahmed2681@users.noreply.github.com>
2023-04-01 01:49:10 +0300

    mmm

--- src/main.js
+++ src/main.js
@@ -1,42 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import Toast from "vue-toastification";
-
-import SuperTokens from "supertokens-web-js";
-import Session from "supertokens-web-js/recipe/session";
-import EmailPassword from "supertokens-web-js/recipe/emailpassword";
-import EmailVerification from "supertokens-web-js/recipe/emailverification";
 
 import App from "./App.vue";
 
 import "./assets/main.css";
-import "vue-toastification/dist/index.css";
 
 import router from "./router";
-
-SuperTokens.init({
-  appInfo: {
-    appName: "Beez Assistant App",
-    apiDomain: "http://localhost:4101",
-    websiteDomain: "http://localhost:3000",
-  },
-  recipeList: [
-    EmailVerification.init({
-      mode: "REQUIRED",
-    }),
-    EmailPassword.init(),
-    Session.init(),
-  ],
-});
 
 const app = createApp(App);
 
 app.use(createPinia());
-
-app.use(Toast, {
-  maxToasts: 1,
-  timeout: 2000,
-});
 
 app.use(router);
 
